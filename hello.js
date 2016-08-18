511dba1909dd120a151695bbc3fc028e19975ed6
René Filip <renefilip@mail.com>
2016-08-18 21:36:06 +0200

    fix hello.js errors

--- hello.js
+++ hello.js
@@ -9,23 +9,22 @@ var pushoverUser = process.env.PUSHOVER_USER;
 var pushoverMessage = "Hello René :)"
 
 var timeBetweenHello = 5 * 60 * 1000
+var lastHello = Date.now() - timeBetweenHello;
 
-var lastHello = Date.now()
-
-app.get('/', function (req, res)) {
-  res.send("Go to /hello to send René a Hello!");
-}
+app.get('/', function (req, res) {
+  res.send("Go to <a href=\"/hello\">Hello</a> to send René a Hello!");
+});
 
 app.get('/hello', function (req, res) {
   var currentTime = Date.now();
 
   if(currentTime - lastHello < timeBetweenHello) {
-    res.send("Sorry! Someone else already said Hello to René :( Maybe you could try it in 5 minutes again?");
+    res.send("Sorry! Someone else already said Hello to René :( Maybe you could try it again in 5 minutes?");
     return
   }
 
   lastHello = Date.now();
-  request.post('http://service.com/upload').form({token:pushoverToken, user:pushoverUser, message:pushoverMessage});
+  request.post(pushoverUrl).form({token:pushoverToken, user:pushoverUser, message:pushoverMessage});
   res.send("You just said Hello to René! How awesome is that?");
 });
 
